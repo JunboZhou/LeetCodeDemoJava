1a514034d263602fa44ed89c30c498e54315c545
zhoujunbo <zhoujunbo@111.com>
2020-07-21 17:49:24 +0800

    二叉搜索树

---
+++ LeetCodeDemo/src/_06_二叉搜索树/BinarySearchTree.java
@@ -0,0 +1,196 @@
+package _06_二叉搜索树;
+
+import _06_二叉搜索树.printer.BinaryTreeInfo;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import javax.sound.midi.Soundbank;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinarySearchTree<E> implements BinaryTreeInfo {
+    private int size;
+    private Node<E> root;
+    private Comparator<E> comparator;
+
+    public BinarySearchTree() {
+        this(null);
+    }
+
+    public BinarySearchTree(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    public void add(E element) {
+        // 添加的是第一个节点
+        if (root == null) {
+            root = new Node<>(element, null);
+            size++ ;
+            return;
+        }
+        // 添加的是不会第一个节点
+        // 找到父节点
+        Node<E> parent = root;
+        Node<E> node = root;
+        int cmp = 0;
+        do {
+            cmp = compare(element, node.element);
+            parent = node;
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else {  // 元素相等
+                node.element = element;
+            }
+        } while (node != null);
+
+        // 查找插入父节点的那个位置
+        Node<E> newNode = new Node<>(element, parent);
+        if (cmp > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+        size++ ;
+    }
+//
+//    public void remove(E element) {
+//
+//    }
+//
+//    public boolean contains(E element) {
+//
+//    }
+//
+//    private void remove(Node<E> node) {
+//
+//    }
+//
+//    private Node<E> node(E element) {
+//
+//    }
+
+
+    /*
+    // 前序遍历
+    public void preorderTraversal() {
+        preorderTraversal(root);
+    }
+
+    private void preorderTraversal(Node<E> node) {
+        if (node == null) return;
+        System.out.println(node.element);
+        preorderTraversal(node.left);
+        preorderTraversal(node.right);
+    }
+
+    // 中序遍历
+    public void inorderTraversal() {
+        inorderTraversal(root);
+    }
+
+    private void inorderTraversal(Node<E> node) {
+        if (node == null) return;
+        inorderTraversal(node.left);
+        System.out.println(node.element);
+        inorderTraversal(node.right);
+    }
+
+    // 后续遍历
+    public void postorderTraversal() {
+        postorderTraversal(root);
+    }
+
+    private void postorderTraversal(Node<E> node) {
+        if (node == null) return;
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+        System.out.println(node.element);
+    }
+
+    // 层序遍历
+    public void levelOrderTraversal() {
+        if (root == null) return;
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Node<E> node = queue.poll();
+            System.out.println(node.element);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+    }
+   */
+    /**
+     * @return 返回值等于0，代表e1和e2相等；返回值大于0，代表e1大于e2；返回值小于于0，代表e1小于e2
+     */
+    private int compare(E e1, E e2) {
+        if (comparator != null) {
+            return comparator.compare(e1, e2);
+        }
+        return ((Comparable<E>) e1).compareTo(e2);
+    }
+
+    @Override
+    public Object root() {
+        return root;
+    }
+
+    @Override
+    public Object left(Object node) {
+        return ((Node<E>)node).left;
+    }
+
+    @Override
+    public Object right(Object node) {
+        return ((Node<E>)node).right;
+    }
+
+    @Override
+    public Object string(Object node) {
+        Node<E> myNode = (Node<E>) node;
+        String parentString = "null";
+        if (myNode.parent != null) {
+            parentString = myNode.parent.element.toString();
+        }
+        return myNode.element + "_p(" + parentString + ")";
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> left;
+        Node<E> right;
+        Node<E> parent;
+
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+        // 叶子节点
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
+
+        public boolean hasTwoChildren() {
+            return left != null && right != null;
+        }
+    }
+}
---
+++ LeetCodeDemo/src/_06_二叉搜索树/MainTest.java
@@ -0,0 +1,48 @@
+package _06_二叉搜索树;
+
+import _06_二叉搜索树.printer.BinaryTrees;
+
+import java.util.Comparator;
+
+public class MainTest {
+    public static void main(String[] args) {
+
+        test1();
+    }
+
+    static void test1() {
+        Integer data[] = new Integer[] {
+                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+        };
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst.add(data[i]);
+        }
+
+        BinaryTrees.println(bst);;
+        bst.preorderTraversal();
+    }
+
+    static void test2() {
+        Integer data[] = new Integer[] {
+                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+        };
+
+        BinarySearchTree<Person> bst1 = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst1.add(new Person(data[i]));
+        }
+
+        BinaryTrees.println(bst1);
+
+        BinarySearchTree<Person> bst2 = new BinarySearchTree<>(new Comparator<Person>() {
+            public int compare(Person o1, Person o2) {
+                return o2.getAge() - o1.getAge();
+            }
+        });
+        for (int i = 0; i < data.length; i++) {
+            bst2.add(new Person(data[i]));
+        }
+        BinaryTrees.println(bst2);
+    }
+}
